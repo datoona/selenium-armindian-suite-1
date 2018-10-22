dd27d543daf03eb92dab779009f390e3c8d24210
Sargis Sargsyan <sargis.sargsyan@live.com>
2018-10-22 21:24:42 +0400

    run tests on firefox

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,7 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
@@ -10,11 +12,15 @@ import static org.testng.Assert.assertTrue;
 public class LoginTest {
     private WebDriver driver;
 
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.gecko.driver",
+                "/Users/sargis/dev/selenium-drivers/geckodriver");
+        driver = new FirefoxDriver();
+    }
+
     @Test
     public void successLogin() {
-        System.setProperty("webdriver.chrome.driver",
-                "/Users/sargis/dev/selenium-drivers/chromedrxiver");
-        driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
         assertTrue(driver.getCurrentUrl().contains("https://www.google.com"),
@@ -24,20 +30,14 @@ public class LoginTest {
 
     @Test
     public void githubFailedLogin() {
-        System.setProperty("webdriver.chrome.driver",
-                "/Users/sargis/dev/selenium-drivers/chromedriver");
-        driver = new ChromeDriver();
-
         LoginPage loginPage =  new LoginPage(driver);
         loginPage.loginWith("mail@mail.am", "Password");
 
         assertTrue(loginPage.isErrorMessageDisplayed(), "Error message was not displayed!");
     }
 
-
     @AfterMethod
     public void tearDown() {
         driver.quit();
-
     }
 }
