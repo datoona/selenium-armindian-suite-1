cdcfbc93eba6c77c48776db62268b7aa0ebbdcf0
Sargis Sargsyan <sargis.sargsyan@live.com>
2018-11-19 20:28:51 +0400

    login moved to selenium base

--- src/test/java/SeleniumBase.java
+++ src/test/java/SeleniumBase.java
@@ -1,10 +1,30 @@
+import api.Client;
 import base.DriverHelper;
+import com.google.gson.JsonObject;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+
+import java.io.IOException;
 
 
 public class SeleniumBase {
+    private WebDriver driver = DriverHelper.get().getDriver();
+
     @AfterMethod
     public void tearDown() {
         DriverHelper.get().quitDriver(DriverHelper.get().getDriver());
     }
+
+    public void login(String username, String password) throws IOException {
+        JsonObject loginJson = Client.login(username, password);
+
+        ((JavascriptExecutor) driver)
+                .executeScript(
+                        "window.localStorage.setItem('token','" + loginJson.get("auth_token") + "');");
+        ((JavascriptExecutor) driver)
+                .executeScript("window.localStorage.setItem('userInfo','" + loginJson + "');");
+
+    }
+
 }
--- src/test/java/TaigaLioginTest.java
+++ src/test/java/TaigaLioginTest.java
@@ -9,20 +9,12 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class TaigaLioginTest extends SeleniumBase {
-    private WebDriver driver = DriverHelper.get().getDriver();
 
     @Test
     public void loginViaApi() throws IOException {
         TaigaLoginPage taigaLoginPage = new TaigaLoginPage();
-        JsonObject loginJson = Client.login("sqa.days@yandex.ru", "Armenia2018");
-
-        ((JavascriptExecutor) driver)
-                .executeScript(
-                        "window.localStorage.setItem('token','" + loginJson.get("auth_token") + "');");
-        ((JavascriptExecutor) driver)
-                .executeScript("window.localStorage.setItem('userInfo','" + loginJson + "');");
-
-        taigaLoginPage = new TaigaLoginPage();
+        login("sqa.days@yandex.ru", "Armenia2018");
+         taigaLoginPage = new TaigaLoginPage();
 
     }
 
